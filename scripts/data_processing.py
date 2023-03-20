5e35e941cd115c65ff3fcbd360e76b35d27c2241
Vikram Grewal <grewav2@student.ubc.ca>
2023-03-19 18:17:53 -0700

    reduced data size for deployment

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -11,10 +11,11 @@ processed_path = '../data/processed/'
 # Loop over raw files, add column names, save to new directory
 for count, file in enumerate(raw_files):
     df = pd.read_csv(file,
-                        names = ['Time (s)', 'Speed (m/s)', 'Gear', 'Engine Load (% of Max Power)', 'Total Acc (m/s^2)', 
-                                'Engine RPM', 'Pitch (deg)', 'Lateral Acc (m/s^2)', 'Passenger Count', 'Load Ind', 
-                                'AC Level', 'Window Opening', 'Radio Volume', 'Rain Intensity', 'Visibility', 
-                                'Driver Wellbeing', 'Driver Rush'],
+                     nrows=50000,
+                     names = ['Time (s)', 'Speed (m/s)', 'Gear', 'Engine Load (% of Max Power)', 'Total Acc (m/s^2)', 
+                              'Engine RPM', 'Pitch (deg)', 'Lateral Acc (m/s^2)', 'Passenger Count', 'Load Ind', 
+                              'AC Level', 'Window Opening', 'Radio Volume', 'Rain Intensity', 'Visibility', 
+                              'Driver Wellbeing', 'Driver Rush'],
                         )
     df = df.set_index('Time (s)')
     if count+1 < 10:
